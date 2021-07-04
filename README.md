407b1c88e96451eef4f071076a644b7a161dfbf4
Carlos Hernandez <khemtz@gmail.com>
2021-07-04 00:50:18 -0500

    add some documetation on README file

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # python_challenges
 Some examples that will help to understand some basic features on python
+
+
+##validate_ip.py
+
+On this script will takes the value from the args, some examples for run:
+```
+	$./validate_ip.py 172.16.23.14 
+	$./validate_ip.py 2001:0db8:85a3:0000:0000:8a2e:0370:7334
+	$./validate_ip.py 11110000.11110000.11110000.11110000 
+	$./validate_ip.py "11110000 11110000 11110000 11110000" 
+	$./validate_ip.py 1111000011110000.1111000011110000.1111000011110000.1111000011110000.1111000011110000.1111000011110000.1111000011110000.1111000011110000
+	$./validate_ip.py "1111000011110000 1111000011110000 1111000011110000 1111000011110000 1111000011110000 1111000011110000 1111000011110000 1111000011110000"
+```
+
+##pythagoran.py
+(https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Trigonometria_01a.svg/1200px-Trigonometria_01a.svg.png)
--- validate_ip.py
+++ validate_ip.py
@@ -6,8 +6,11 @@ import re,sys
 print (str(len(sys.argv)))
 var1 = sys.argv[1]
 
+#we need to verify that ipv4 has values between 0 to 255 with the regexp taken from logstash grok patterns, make some modifications:
 #us_ipv4 = "(?<![0-9])(?:(?:[0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])[.](?:[0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])[.](?:[0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])[.](?:[0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5]))(?![0-9])"
 us_ipv4 = "^(?:(?:([0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])(\.)){3}(?:([0-1]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])))$"
+
+#for IPv6 we need to verify is a valid HEX format. 
 
 us_ipv6 = "((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?$"
 
@@ -16,22 +19,13 @@ us_ipv6 = "((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}
 us_ipv4binary = "^(?:[01]{8}(\.|\s)){3}[01]{8}$"
 us_ipv6binary = "^(?:[01]{16}(:|\.|\s)){7}[01]{16}$"
 
-#for the next regexp i assume that for binary the value must be on 8 bits separate by \. 
-
-us_ipv4oct = "^([0-7]{4}(\.|\s)){3}[0-7]{4}$"
-us_ipv6oct = "^([0-7]{8}(:|\.|\s)){7}[0-7]{8}$"
-
 if re.match(us_ipv4,var1):
     print("is a valid IPv4 IP: " + var1)
 elif re.match(us_ipv6,var1):
     print("is a valid IPv6 IP: " + var1)
 elif re.match(us_ipv4binary,var1):
     print("is a valid IPv4 Binary IP: " + var1)
-elif re.match(us_ipv4oct,var1):
-    print("is a valid IPv4 Octal: " + var1)
 elif re.match(us_ipv6binary,var1):
     print("is a valid IPv6 Binary IP: " + var1)
-elif re.match(us_ipv6oct,var1):
-    print("is a valid IPv6 Octal: " + var1)
 else:
     print("is not valid IP")
