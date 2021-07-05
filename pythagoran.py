273257a084c9dc1ab5052ae0253735ed1391af8e
Carlos Hernandez <khemtz@gmail.com>
2021-07-05 11:49:35 -0500

    modify pythagorian script for optimize

--- pythagoran.py
+++ pythagoran.py
@@ -1,12 +1,10 @@
 #! /usr/bin/python3 
 import math
-def get_pythagoranValue(a,b,c):
-    return (True if c == math.sqrt(a * a + b * b) else False)
 
 str_input = input("Bring 3 Integer Numbers (separate by space):")
 n = str_input.split(" ") + str_input.split(" ")
-if (len(n) > 4):
-   values = [True if get_pythagoranValue(float(n[i+1]),float(n[i+2]),float(n[i])) else False for i in range(3)]
+if (len(n) == 6):
+   values = [True if float(n[i]) == math.sqrt((float(n[i+1]) * float(n[i+1])) + (float(n[i+2]) * float(n[i+2]))) else False for i in range(3)]
    try:
       i=values.index(True);
       print ("These numbers are a valid pythagorean triplet when:\n  A (side)=" + str(int(n[i+1])) + "\n  B (side)=" + str(int(n[i+2])) + "\n  C (hypotenuse)=" + str(int(n[i])))
